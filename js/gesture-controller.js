187d19adfa9c2129a21dff7cd29a0b5f94d1f304
Paul <paulb896@gmail.com>
2014-07-20 22:00:29 -0700

    Clean controller code

--- js/gesture-controller.js
+++ js/gesture-controller.js
@@ -13,15 +13,12 @@ angular.module('gestureApp.gestureController', ['underscore'])
      * Setup page.
      */
     $scope.initialize = function() {
-        var collection = [
+        $scope.collectionInformation = {};
+        $scope.collectionInformation.list = [
             {id: 1, name:"Ghostbusters", url:"http://www.emoticonswallpapers.com/avatar/games/Ghostbusters-2.gif"},
             {id: 2, name:"SSX", url:"http://a.psblogstatics.com/wp-content/uploads/avatars/avatar_395946.jpg?m=1327688931"},
             {id: 3, name:"NBA JAM", url:"http://screenshot.it.sftcdn.net/it/scrn/3344000/3344013/nba-jam-07-100x100.png"}
         ];
-
-        $scope.collectionInformation = {};
-        $scope.collectionInformation.list = collection;
-        $scope.type = "Nothing yet";
     };
 
     $scope.addItem = function(item) {
